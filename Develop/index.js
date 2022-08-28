f07dd670cde62284c487c3df1342c0bfa06bc80d
P Luong <p-luong@live.co.uk>
2022-08-28 14:04:50 +0100

    added packages required

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,49 @@
 // TODO: Include packages needed for this application
+const generateMarkdown = require('./utils/generateMarkdown.js')
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'input',
+      name: 'location',
+      message: 'Where are you from?',
+    },
+    {
+      type: 'input',
+      name: 'hobby',
+      message: 'What is your favorite hobby?',
+    },
+    {
+      type: 'input',
+      name: 'food',
+      message: 'What is your favorite food?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
+    },
+    {
+      type: 'input',
+      name: 'linkedin',
+      message: 'Enter your LinkedIn URL.',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
